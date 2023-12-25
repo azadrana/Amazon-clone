1c63df0c9129442e06e5f2d3364c0c8484aae17c
azad rana <kazad9084@gmail.com>
2023-12-25 14:58:50 +0530

    first commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home'
+import { BrowserRouter as Router, Switch, Routes, Route } from 'react-router-dom'
+import Checkout from './Checkout';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout />} />
+
+          <Route  exact path='/' element={<Home/>} />
+         
+
+        </Routes>
+
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
